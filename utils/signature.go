5b23154e4f155d8650d1289569ba6b7e148a643a
smallyunet <dev.wangyu@gmail.com>
2022-08-23 17:10:48 +0800

    add sign by eth func

--- utils/signature.go
+++ utils/signature.go
@@ -1,8 +1,11 @@
 package utils
 
 import (
+	"crypto/elliptic"
 	"errors"
 	"fmt"
+	"github.com/saveio/themis/common/log"
+	"github.com/saveio/themis/crypto/ec"
 
 	"github.com/saveio/themis/account"
 	"github.com/saveio/themis/crypto/keypair"
@@ -12,6 +15,26 @@ import (
 func Sign(acc *account.Account, data []byte) ([]byte, error) {
 	sig, err := s.Sign(acc.SigScheme, acc.PrivateKey, data, nil)
 	if err != nil {
+		return nil, err
+	}
+	sigData, err := s.Serialize(sig)
+	if err != nil {
+		return nil, fmt.Errorf("signature.Serialize error:%s", err)
+	}
+	return sigData, nil
+}
+
+func SignByETH(acc *account.Account, data []byte) ([]byte, error) {
+	keyBytes := acc.GetEthPrivateKey()
+	ecdsaKey := ec.ConstructPrivateKey(keyBytes, elliptic.P256())
+	priKey := &ec.PrivateKey{
+		Algorithm:  ec.ECDSA,
+		Raw:        keyBytes,
+		PrivateKey: ecdsaKey,
+	}
+	sig, err := s.Sign(acc.SigScheme, priKey, data, nil)
+	if err != nil {
+		log.Debugf("SignByETH error:%s", err)
 		return nil, err
 	}
 	sigData, err := s.Serialize(sig)
